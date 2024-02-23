8347f9c4fcdb68d3a4ac7e2d27547bcf6c0da3e8
Nurus Memmedzade <nurusmammadzada@gmail.com>
2024-02-23 18:42:53 +0400

    first python project.Simple example no1

---
+++ python.py
@@ -0,0 +1,22 @@
+import turtle
+a = turtle.Turtle()
+a.speed()
+a.getscreen().bgcolor("black")
+a.penup()
+a.pendown()
+a.color("red")
+a.speed(25)
+def star(turtle, size):
+    if size <= 10:
+        return
+    else:
+        turtle.begin_fill()
+        for i in range(5):
+            turtle.pensize(2)
+            turtle.forward(size)
+            star(turtle, size/3)
+            turtle.left(216)
+            turtle.end_fill()
+star(a, 360) 
+turtle.done()
+
